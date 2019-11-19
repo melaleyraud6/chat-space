9b18ec6044ab65580a69b9bc474d34e16da0d2bb
melaleyraud6 <theflare.akg.9pb@gmail.com>
2019-11-19 17:56:37 +0900

    Ajax edit3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
     .done(function(message){
       var html = buildMessage(message);
       $('.messages').append(html);
-      $("#new_message").reset('');
+      $("#new_message")[0].reset('');
       $('.submit-btn').prop('disabled', false);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })

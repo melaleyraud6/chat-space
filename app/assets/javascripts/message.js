1a0bb19903d61f3b53c62d8553a73ce3616b7248
melaleyraud6 <theflare.akg.9pb@gmail.com>
2019-11-19 14:25:09 +0900

    Ajax-chat-space

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+$(function(){
+  function buildMessage(message){
+    var body = message.body ? `${message.body}` : "";
+    var image = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="message">
+    <div class="message__upper-info">
+    <div class="message__upper-info__talker">
+    ${message.user_name}
+    </div>
+    <div class="message__upper-info__date">
+    ${message.created_at}
+    </div>
+    </div>
+    <div class="message_text">
+    <p class="message_text__content">
+      <div>
+      ${body}
+      </div>
+      ${image}
+    </p>
+    
+    </div>
+    </div>`
+    return html;
+  }
+
+  $("#new_message").on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message);
+      $('.messages').append(html);
+      $("#message_body").val('');
+      $('.submit-btn').prop('disabled', false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(){
+       alert("メッセージ送信に失敗しました");
+    })
+  })
+});
